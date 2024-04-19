47d9e191bff0fbf51cd3b414653aabcc9cf9880b
Karolina Meira <karolina.meira@hotmail.com>
2024-04-19 20:36:06 -0300

    Adicionando alerts personalizados

--- blog-pessoal/src/components/navbar/Navbar.tsx
+++ blog-pessoal/src/components/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import { Link, useNavigate } from 'react-router-dom'
 import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../contexts/AuthContext'
 
 
 function Navbar() {
-    let navigate = useNavigate()
+  let navigate = useNavigate()
 
     const { usuario, handleLogout } = useContext(AuthContext)
 
@@ -16,25 +16,29 @@ function Navbar() {
 
     let navbarComponent
 
-    return (
-        <>
-            <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-                <div className="container flex justify-between text-lg">
-                    <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+    if(usuario.token !== "") {
+      navbarComponent = (
+        <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <Link to='/home' className='text-2xl font-bold uppercase'>Blog Pessoal</Link>
 
-                    <div className='flex gap-4'>
-                        <Link to='/login' className='hover:underline'>Login</Link>
-                        <Link to='/home' className='hover:underline'>Home</Link>
-                        <Link to='/postagens' className='hover:underline'>Postagens</Link>
-                        <Link to='/temas' className='hover:underline'>Temas</Link>
-                        <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
-                        <div className='hover:underline'>Perfil</div>
-                        <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-                    </div>
-                </div>
+            <div className='flex gap-4'>
+              <Link to='/postagens' className='hover:underline'>Postagens</Link>
+              <Link to='/temas' className='hover:underline'>Temas</Link>
+              <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
+              <Link to='/perfil' className='hover:underline'>Perfil</Link>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      )
+    }
+
+  return (
+    <>
+      {navbarComponent}
+    </>
+  )
 }
 
 export default Navbar
--- blog-pessoal/src/components/temas/listaTemas/ListaTemas.tsx
+++ blog-pessoal/src/components/temas/listaTemas/ListaTemas.tsx
@@ -5,11 +5,12 @@ import { AuthContext } from '../../../contexts/AuthContext';
 import Tema from '../../../models/Tema';
 import { buscar } from '../../../services/Service';
 import CardTemas from '../cardTemas/CardTemas';
+import { toastAlerta } from '../../../util/toastAlerta';
 
 function ListaTemas() {
   const [temas, setTemas] = useState<Tema[]>([]);
 
-  const navigate = useNavigate();
+  let navigate = useNavigate();
 
   const { usuario, handleLogout } = useContext(AuthContext);
   const token = usuario.token;
@@ -20,8 +21,8 @@ function ListaTemas() {
         headers: { Authorization: token },
       });
     } catch (error: any) {
-      if (error.toString().includes('403')) {
-        alert('O token expirou, favor logar novamente')
+      if(error.toString().includes('403')) {
+        toastAlerta('O token expirou, favor logar novamente', 'info')
         handleLogout()
       }
     }
@@ -29,7 +30,7 @@ function ListaTemas() {
 
   useEffect(() => {
     if (token === '') {
-      alert('Você precisa estar logado');
+      toastAlerta('Você precisa estar logado', 'info');
       navigate('/login');
     }
   }, [token]);
@@ -37,7 +38,6 @@ function ListaTemas() {
   useEffect(() => {
     buscarTemas();
   }, [temas.length]);
-
   return (
     <>
       {temas.length === 0 && (
@@ -53,7 +53,6 @@ function ListaTemas() {
       <div className="flex justify-center w-full my-4">
         <div className="container flex flex-col">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            
             {temas.map((tema) => (
               <>
                 <CardTemas key={tema.id} tema={tema} />
@@ -65,4 +64,5 @@ function ListaTemas() {
     </>
   );
 }
+
 export default ListaTemas;
